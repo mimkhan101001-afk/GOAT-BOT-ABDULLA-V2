3f79c49dc0dad318de1d8eb0464a4229919678d9
mimkhan101001-afk <mimkhan101001@gmail.com>
2025-10-21 17:37:12 +0600

    Update text_voice.js

--- scripts/cmds/text_voice.js
+++ scripts/cmds/text_voice.js
@@ -6,7 +6,7 @@ module.exports = {
   config: {
     name: "text_voice",
     version: "1.0.0",
-    author: "MOHAMMAD AKASH",
+    author: "Mehedi Hassan",
     countDown: 5,
     role: 0,
     shortDescription: "নির্দিষ্ট টেক্সটে ভয়েস রিপ্লাই 😍",
