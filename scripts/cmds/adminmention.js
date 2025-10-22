f01e8770ddd54ade80142e842903020bdc1a41a8
mimkhan101001-afk <mimkhan101001@gmail.com>
2025-10-22 12:37:43 +0600

    Update adminmention.js

--- scripts/cmds/adminmention.js
+++ scripts/cmds/adminmention.js
@@ -13,7 +13,7 @@ module.exports = {
   onStart: async function () {},
 
   onChat: async function ({ event, message }) {
-    const adminIDs = ["mehedihassan10012", "mehedihassan10012", "mehedihassan10012"].map(String);
+    const adminIDs = ["61581500445402", "61581500445402", "61581974807041"].map(String);
     // Skip if sender is admin
     if (adminIDs.includes(String(event.senderID))) return;
 
